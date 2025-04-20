149f5ac17e967298dd8a02908807f922cc167789
Julio H. <juliohenricordeiro@gmail.com>
2025-04-20 18:32:13 -0300

    Implementa função main com exibição de dados institucionais do aluno na disciplina de Devops

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,19 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+fun main() {
+
+    val aluno = "Julio H."
+    val instituicao = "Pontifícia Universidade Católica do Paraná"
+    val curso = "Análise e Desenvolvimento de Sistemas"
+    val disciplina = "DevOps"
+    val professor = "Felipe Elias"
+    var nota = 0.0
+
+    println("Aluno: ${aluno}\n" +
+            "Instituição: ${instituicao}\n" +
+            "Curso: ${curso}\n" +
+            "Disciplina: ${disciplina}\n" +
+            "Professor: ${professor}\n" +
+            "Nota do Aluno: ${nota}")
 }
